19eab943fe5925f2f453331b4926f7e599bcd7b5
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2013-10-20 15:51:20 +0200

    sony: common: cleanup offline charger resources
    Change-Id: I0e3f6a74c7afec7f546ed5d4a6a32ae4eeca99d6

---
+++ resources.mk
@@ -0,0 +1,20 @@
+# Offline charging animation
+PRODUCT_COPY_FILES += \
+     device/sony/common/animations/charging_animation_01.png:system/semc/chargemon/data/charging_animation_01.png \
+     device/sony/common/animations/charging_animation_02.png:system/semc/chargemon/data/charging_animation_02.png \
+     device/sony/common/animations/charging_animation_03.png:system/semc/chargemon/data/charging_animation_03.png \
+     device/sony/common/animations/charging_animation_04.png:system/semc/chargemon/data/charging_animation_04.png \
+     device/sony/common/animations/charging_animation_05.png:system/semc/chargemon/data/charging_animation_05.png \
+     device/sony/common/animations/charging_animation_06.png:system/semc/chargemon/data/charging_animation_06.png \
+     device/sony/common/animations/charging_animation_07.png:system/semc/chargemon/data/charging_animation_07.png \
+     device/sony/common/animations/non-charging_animation_01.png:system/semc/chargemon/data/non-charging_animation_01.png \
+     device/sony/common/animations/non-charging_animation_02.png:system/semc/chargemon/data/non-charging_animation_02.png \
+     device/sony/common/animations/non-charging_animation_03.png:system/semc/chargemon/data/non-charging_animation_03.png \
+     device/sony/common/animations/non-charging_animation_04.png:system/semc/chargemon/data/non-charging_animation_04.png \
+     device/sony/common/animations/non-charging_animation_05.png:system/semc/chargemon/data/non-charging_animation_05.png \
+     device/sony/common/animations/non-charging_animation_06.png:system/semc/chargemon/data/non-charging_animation_06.png \
+     device/sony/common/animations/non-charging_animation_07.png:system/semc/chargemon/data/non-charging_animation_07.png
+
+# Boot Logo
+PRODUCT_COPY_FILES += \
+     device/sony/common/bootlogo/$(TARGET_SCREEN_WIDTH)x$(TARGET_SCREEN_HEIGHT).rle:root/logo.rle
