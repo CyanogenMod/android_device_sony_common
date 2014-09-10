a86009ef2f5dee762c97285ca29bbbb97dd35a0d
Chirayu Desai <cdesai@cyanogenmod.org>
2014-09-10 09:05:38 +0530

    Build setup_fs
    * Used for formatting data after unlock,
      as the userdata partition is zeroed out after unlock,
      but a filesystem isn't created.
    
    Change-Id: I17aba9ed9778431e48f9605d10a7fde8be9b7b86

--- resources.mk
+++ resources.mk
@@ -23,3 +23,7 @@ else
 PRODUCT_COPY_FILES += \
      device/sony/common/bootlogo/fxp/$(TARGET_SCREEN_WIDTH)x$(TARGET_SCREEN_HEIGHT).rle:root/logo.rle
 endif
+
+# For formatting data after unlock
+PRODUCT_PACKAGES += \
+    setup_fs
