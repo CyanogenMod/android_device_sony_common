b59a9878541d70dd48ea4532f834f288171f1343
Chirayu Desai <chirayudesai1@gmail.com>
2014-11-09 09:14:11 +0530

    extract_elf_ramdisk: update elfutils path
    Change-Id: Ie00d1f5f7bc9ce44fe582d3dbcaa62a665723281

--- extract_elf_ramdisk/Android.mk
+++ extract_elf_ramdisk/Android.mk
@@ -5,7 +5,7 @@ LOCAL_SRC_FILES := extract_elf_ramdisk.c
 LOCAL_FORCE_STATIC_EXECUTABLE := true
 LOCAL_STATIC_LIBRARIES := libelf libc libm libz
 LOCAL_C_INCLUDES := \
-	external/elfutils/libelf \
+	external/elfutils/0.153/libelf \
 	external/zlib
 LOCAL_CFLAGS := -g -c -W
 LOCAL_MODULE := extract_elf_ramdisk
