bfbe3ce7933a7103138ca6019bac2e24f2a25db0
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2014-11-24 21:38:04 +0100

    make charger and logo opt-in
    Change-Id: If3c1d55591d191d5eb503aae6a434a62d1610668

--- resources.mk
+++ resources.mk
@@ -1,3 +1,4 @@
+ifeq ($(TARGET_REQUIRE_SONY_CHARGER),true)
 # Offline charging animation
 PRODUCT_COPY_FILES += \
      device/sony/common/animations/charging_animation_01.png:system/semc/chargemon/data/charging_animation_01.png \
@@ -14,14 +15,17 @@ PRODUCT_COPY_FILES += \
      device/sony/common/animations/non-charging_animation_05.png:system/semc/chargemon/data/non-charging_animation_05.png \
      device/sony/common/animations/non-charging_animation_06.png:system/semc/chargemon/data/non-charging_animation_06.png \
      device/sony/common/animations/non-charging_animation_07.png:system/semc/chargemon/data/non-charging_animation_07.png
+endif
 
 # Boot Logo
+ifeq ($(TARGET_REQUIRE_LOGO),true)
 ifneq ($(FXP_BUILD),true)
 PRODUCT_COPY_FILES += \
      device/sony/common/bootlogo/$(TARGET_SCREEN_WIDTH)x$(TARGET_SCREEN_HEIGHT).rle:root/logo.rle
 else
 PRODUCT_COPY_FILES += \
      device/sony/common/bootlogo/fxp/$(TARGET_SCREEN_WIDTH)x$(TARGET_SCREEN_HEIGHT).rle:root/logo.rle
+endif
 endif
 
 # For formatting data after unlock
