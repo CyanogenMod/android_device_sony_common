41f12210e2207761d9b0c221fc8baaa0e47ac2d2
AdrianDC <radian.dc@gmail.com>
2015-11-22 02:05:28 +0100

    sony-common: Delete old chargemon resources
     * No longer used by CM supported devices, use healthd instead
    
    Change-Id: Ifd856e278dd238b3c5630065a3175e490160fef7
    Signed-off-by: AdrianDC <radian.dc@gmail.com>

--- resources.mk
+++ resources.mk
@@ -1,20 +1,3 @@
-# Offline charging animation
-PRODUCT_COPY_FILES += \
-     device/sony/common/animations/charging_animation_01.png:system/semc/chargemon/data/charging_animation_01.png \
-     device/sony/common/animations/charging_animation_02.png:system/semc/chargemon/data/charging_animation_02.png \
-     device/sony/common/animations/charging_animation_03.png:system/semc/chargemon/data/charging_animation_03.png \
-     device/sony/common/animations/charging_animation_04.png:system/semc/chargemon/data/charging_animation_04.png \
-     device/sony/common/animations/charging_animation_05.png:system/semc/chargemon/data/charging_animation_05.png \
-     device/sony/common/animations/charging_animation_06.png:system/semc/chargemon/data/charging_animation_06.png \
-     device/sony/common/animations/charging_animation_07.png:system/semc/chargemon/data/charging_animation_07.png \
-     device/sony/common/animations/non-charging_animation_01.png:system/semc/chargemon/data/non-charging_animation_01.png \
-     device/sony/common/animations/non-charging_animation_02.png:system/semc/chargemon/data/non-charging_animation_02.png \
-     device/sony/common/animations/non-charging_animation_03.png:system/semc/chargemon/data/non-charging_animation_03.png \
-     device/sony/common/animations/non-charging_animation_04.png:system/semc/chargemon/data/non-charging_animation_04.png \
-     device/sony/common/animations/non-charging_animation_05.png:system/semc/chargemon/data/non-charging_animation_05.png \
-     device/sony/common/animations/non-charging_animation_06.png:system/semc/chargemon/data/non-charging_animation_06.png \
-     device/sony/common/animations/non-charging_animation_07.png:system/semc/chargemon/data/non-charging_animation_07.png
-
 # Boot Logo
 ifneq ($(FXP_BUILD),true)
 PRODUCT_COPY_FILES += \
