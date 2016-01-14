0e31334a2e1ff277c3fb103ed37358ec1a6500f5
AdrianDC <radian.dc@gmail.com>
2016-01-14 23:07:04 +0100

    sony: common: Cleanup extract_ramdisk & allow C++ source inclusion
     * Updated to compile in C++
     * Cleanup typecast errors / warnings
     * Fix missmatching array tables
     * Remove unused variables
     * Remove unproper close(img_fd)
     * Rename to extract_ramdisk
    
    Change-Id: I25481f0566288a8176dd5f464d3102d23fe0397b
    Signed-off-by: AdrianDC <radian.dc@gmail.com>

--- extract_ramdisk/Android.mk
+++ extract_ramdisk/Android.mk
@@ -1,14 +1,14 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := extract_elf_ramdisk.c
+LOCAL_SRC_FILES := extract_ramdisk.cpp main.cpp
 LOCAL_FORCE_STATIC_EXECUTABLE := true
 LOCAL_STATIC_LIBRARIES := libelf libc libm libz
 LOCAL_C_INCLUDES := \
 	external/elfutils/0.153/libelf \
 	external/zlib
-LOCAL_CFLAGS := -g -c -W
-LOCAL_MODULE := extract_elf_ramdisk
+LOCAL_CPPFLAGS := -g -c -W
+LOCAL_MODULE := extract_ramdisk
 LOCAL_MODULE_TAGS := eng
 LOCAL_MODULE_CLASS := RECOVERY_EXECUTABLES
 LOCAL_MODULE_PATH := $(PRODUCT_OUT)/utilities
