1e61e76b403a58e054c6ef08b0938df286997d34
Adam Farden <adam@farden.cz>
2016-01-22 14:05:28 +0100

    init: Add support for 'no-keycheck' variant
     * AOSP supported SONY devices require only warmboot to recovery
     * Allow deactivation of init keycheck with KEYCHECK_ENABLED 0
    
     * Enable 'no-keycheck' variant with following init_board_device.h sources
    
        // Constants: init configurations
        #define KEYCHECK_ENABLED 0
    
        // Class: init_board_device
        class init_board_device : public init_board_common
        {
        public:
            // Board: Finish init
            virtual void finish_init()
            {
                // Rename init.real
                unlink("/init");
                rename("/init.real", "/init");
            }
        };
    
    Change-Id: Ibc07ef80f5e85776b255d52d1018ff7b3443aefa
    Signed-off-by: Adam Farden <adam@farden.cz>

--- init/init_main.cpp
+++ init/init_main.cpp
@@ -72,6 +72,7 @@ int main(int argc, char** __attribute__((unused)) argv)
     // Keycheck introduction
     if (!recoveryBoot)
     {
+#if KEYCHECK_ENABLED
         // Listen for volume keys
         const char* argv_keycheck[] = { EXEC_KEYCHECK, nullptr };
         pid_t keycheck_pid = system_exec_bg(argv_keycheck);
@@ -83,6 +84,7 @@ int main(int argc, char** __attribute__((unused)) argv)
         keycheckStatus = system_exec_kill(keycheck_pid, KEYCHECK_TIMEOUT);
         recoveryBoot = (keycheckStatus == KEYCHECK_RECOVERY_CM_ONLY ||
                 keycheckStatus == KEYCHECK_RECOVERY_FOTA_CM);
+#endif
     }
     else
     {
@@ -101,6 +103,7 @@ int main(int argc, char** __attribute__((unused)) argv)
         if (DEV_BLOCK_FOTA_NUM != -1 &&
                 keycheckStatus != KEYCHECK_RECOVERY_CM_ONLY)
         {
+            write_string(BOOT_TXT, "RECOVERY FOTA " DEV_BLOCK_FOTA_PATH, true);
             mknod(DEV_BLOCK_FOTA_PATH, S_IFBLK | 0600,
                     makedev(DEV_BLOCK_MAJOR, DEV_BLOCK_FOTA_NUM));
             mount("/", "/", NULL, MS_MGC_VAL | MS_REMOUNT, "");
