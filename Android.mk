88d779dcb09be914b48658fb1fafa9a556be8c76
Adam Farden <adam@farden.cz>
2016-02-12 09:30:33 +0100

    common: Clean up and remove unused common sony parts
     * FXP logo is never used
      - All FXP devices use bootloader splash or kernel splash
     * Bootlogo are not used by all Sony devices
      - Devices requiring the logo.rle should add and link in the device tree
     * Remove BoardConfigCommon.mk
      - It was mostly obsolete anyway
      - Move BOARD_CUSTOM_BOOTIMG to platforms that still need it
      - Move BOARD_VENDOR := sony to platforms BoardConfig.mk
      - BoardConfig.mk inherit from BoardConfigCommon.mk has to be removed
     * Remove resources.mk
      - Only setup_fs would remain in it
      - setup_fs has been deprecated in favour to fs_mgr
      - device.mk reference to resources.mk has to be removed
    
    Change-Id: I9f50aac5d6f91a078eff303d1fc609dfc4d8062b
    Signed-off-by: Adam Farden <adam@farden.cz>

--- Android.mk
+++ Android.mk
@@ -1,5 +1,5 @@
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(BOARD_VENDOR),sony)
+ifeq ($(BOARD_VENDOR),Sony)
     include $(call all-subdir-makefiles,$(LOCAL_PATH))
 endif
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Copyright 2013 - The CyanogenMod Project
+Copyright 2013-2016 - The CyanogenMod Project
 
 android_device_sony_common
 ===============================
