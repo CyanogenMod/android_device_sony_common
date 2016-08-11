9ae961b9b9adcf51305b4067fe0e2c247ecf638c
Alexander Diewald <Diewi@diewald-net.com>
2016-08-11 21:21:36 +0200

    init: Use a more conventional ramdisk layout
    The current ramdisk layout for sony devices does not obey to the
    convention where all ramdisk files reside directly within the root of
    the ramdisk (e.g., /init). Instead, the actual content of the Android
    ramdisk is packed in the file '/sbin/ramdisk.cpio' that is extracted
    when performing an Android boot. This is done to also pack a recovery
    ramdisk and allow booting from it.
    
    Unfortunately, due to Google's change in the update mechanism, many
    tools (e.g., SuperSU and Xposed) now work on the ramdisk instead of the
    system partition. This conflicts with the ramdisk layout of the sony
    devices using the init_sony mechanism. Since this layout does not affect
    too many users, the required changes in the tools will probably never go
    upstream and would require patching every single release.
    
    This commit therefore changes the init_sony mechanism to use a
    conventional layout of the ramdisk, while still providing the ability to
    boot from a packed recovery. Also, booting from the FOTA partition is
    still possible. If the user decides to boot into recovery, the ramdisk
    files belonging to the Android ramdisk are removed such that a clean
    state is achieved before unpacking the recovery ramdisk.
    
    For this change to operate correctly, an update to the device
    custombootimg.mk is required, see the commit uploaded separately.
    
    In particular this commit introduces the following changes:
    
     - Introduce a files "starting / ending with" deleter in init_files.cpp
     - Remove the Android ramdisk files before extracting the recovery,
       added to a new init_ramdisk.cpp file listing the ramdisk files
    
    Change-Id: I816b5168d0de9c6987cc3b36fbd659fb2404f94b
    Signed-off-by: Adrian DC <radian.dc@gmail.com>

---
+++ init/init_ramdisk.cpp
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "init_prototypes.h"
+
+// Function: ramdisk contents cleaner
+void ramdisk_clean_files(void)
+{
+    dir_unlink_r("/res", false);
+    file_unlink_wildcard("/", "/fstab", "");
+    file_unlink_wildcard("/", "", ".rc");
+    unlink("/sbin/adbd");
+    unlink("/sbin/healthd");
+    unlink("/sbin/ueventd");
+    unlink("/sbin/watchdogd");
+    unlink("/charger");
+    unlink("/default.prop");
+    unlink("/file_contexts");
+    unlink("/init");
+    unlink("/init.real");
+    unlink("/logo.rle");
+    unlink("/property_contexts");
+    unlink("/seapp_contexts");
+    unlink("/selinux_version");
+    unlink("/sepolicy");
+    unlink("/service_contexts");
+}
