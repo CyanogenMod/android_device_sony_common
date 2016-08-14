4e838f90b24e2402de2eb8a47a3482d1ec7ceb8e
Adrian DC <radian.dc@gmail.com>
2016-08-14 15:36:00 +0200

    init: Clean the init_sony specific binaries on boot
     * keycheck, init_sony and ramdisk-recovery.cpio
        are no longer needed when Android or recovery booted,
        so delete them the same way toybox_init is deleted
    
    RM-290
    
    Change-Id: Id43a4e0f31de02089d657a848f0bc12bf69fcc2d

--- init/init_board_common.h
+++ init/init_board_common.h
@@ -25,6 +25,7 @@
 #define EXEC_TOYBOX "/sbin/toybox_init"
 #define SBIN_CPIO_ANDROID "/sbin/ramdisk.cpio"
 #define SBIN_CPIO_RECOVERY "/sbin/ramdisk-recovery.cpio"
+#define SBIN_INIT_SONY "/sbin/init_sony"
 
 // Constants: keycheck commands
 #define KEYCHECK_RECOVERY_BOOT_ONLY (41 << 8)
